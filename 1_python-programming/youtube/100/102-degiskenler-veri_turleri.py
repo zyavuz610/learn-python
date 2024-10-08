0b15166fbc9b195de9b96a26922fc4eaa69e233c
zyavuz610 <43997998+zyavuz610@users.noreply.github.com>
2024-10-08 22:47:13 +0300

    Create 102-degiskenler-veri_turleri.py

---
+++ 1_python-programming/youtube/100/102-degiskenler-veri_turleri.py
@@ -0,0 +1,42 @@
+sayi1,sayi2 = 10, 25
+toplam = sayi1 + sayi2
+print("Birinci Sayı:",sayi1)
+print("İkinci Sayı:",sayi2)
+print("Toplam:",toplam)
+
+isim = "Zafer"
+print("Merhaba",isim)
+
+
+# Veri Türleri
+
+# Text Type:	str, (string) karakterlerden oluşan veri türüdür, tek tırnak veya çift tırnak kullanılarak ifade edilirler
+soyad = "Yavuz"
+sehir = 'Trabzon'
+
+
+print(sehir)  # sehir, str
+sehir = 61
+print(sehir)  # sehir, int
+
+# Numeric Types:	int, float, complex
+sayi1 = -10 # int, tamsayı (integer)
+s2 = 20.5 # float, kayan noktalı sayı
+c2 = 2 + 3j
+print(c2)
+
+# Sequence Types:	list, tuple, range
+# içerisinde birden çok veri barındıran yapılardır.
+
+# Mapping Type:	dict, sözlük
+
+# Set Types:	set, frozenset, küme
+
+
+# Boolean Type:	bool, doğru yada yanlış değer olabilen veri
+geldi_mi = True
+print("Geldi mi?",geldi_mi)
+
+# Binary Types:	bytes, bytearray, memoryview
+
+# None Type:	NoneType
