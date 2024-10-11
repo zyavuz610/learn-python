de9630c6abb5c938daef089c295b9489666a3103
zyavuz610 <43997998+zyavuz610@users.noreply.github.com>
2024-10-11 23:21:55 +0300

    Create 104-veri-turleri_tur-donusumu.py

---
+++ 1_python-programming/youtube/100/104-veri-turleri_tur-donusumu.py
@@ -0,0 +1,67 @@
+"""
+Seri: Örneklerle Python Programlama ve Algoritma
+
+Python 100 - Python ile programlamaya giriş
+Python 101 - Python ile Ekrana Çıktı Yazmak, print() fonksiyonu
+Python 102 - Değişkenler ve Veri Türleri
+Python 103 - Aritmetik operatörler ve not ortalaması bulma örneği
+
+Python 104 - Sayılar, bool ifadeler ve tür dönüşümü
+
+
+
+Sayılar
+x = -11  # int
+y = 1.8  # float
+z = 2+3j   # complex
+
+tipini öğrenme
+print(type(x))
+
+uç örnekler
+a = 35656222554887711
+x = 35e3  = 35* (10**3)
+y = 12E4
+z = -87.7e100
+x = 3+5j
+y = 5j
+z = -5j
+
+boolean
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+
+Tür Dönüşümü
+int, str, float, complex, bool
+
+
+True değerli ifadeler, içinde değer olan
+bool("abc")
+bool(123)
+bool(["apple", "cherry", "banana"])
+
+False değerli ifadeler, içerisinde değer olmayan
+bool(False)
+bool(None)
+bool(0)
+bool("")
+bool(())
+bool([])
+bool({})
+
+Tip Dönüşümleri
+x = int(1)   # x = 1
+y = int(2.8) # y = 2
+z = int("3") # z = 3
+
+x = float(1)     # x = 1.0
+y = float(2.8)   # y = 2.8
+z = float("3")   # z = 3.0
+w = float("4.2") # w = 4.2
+
+x = str("s1") # x = 's1'
+y = str(2)    # y = '2'
+z = str(3.0)  # z = '3.0'
+
+"""
