77cdac338705511336a5b4ade947060cd464d51f
zyavuz610 <43997998+zyavuz610@users.noreply.github.com>
2024-10-13 13:07:54 +0300

    Create 116-fonksiyonlar-1.py

---
+++ 1_python-programming/youtube/100/116-fonksiyonlar-1.py
@@ -0,0 +1,25 @@
+"""
+Seri: Örneklerle Python Programlama ve Algoritma
+
+Önceki dersler:
+  değişkenler ve operatörler
+  koşul ifadeleri: if,else
+  veri yapıları: string, liste
+  döngüler: for, while, iç içe döngüler
+
+Python 116 - Fonksiyonlar, fonksiyon parametreleri, argümanlar, geri dönüş değeri
+
+fonk. modüler programlama için kullanılan temel kodlama bileşenleridir.
+2 temel görev yapılmalı
+1. fonksiyon tanımlama
+2. o fonksiyonu çağırma (func. call)
+"""
+
+# 1) fonk. tanımlama, fonksiyon ismi greeting olsun
+def greeting():
+  print("Merhaba Dünya!")
+
+# 2) o fonk. çağırma, çalıştırma
+greeting()
+greeting()
+greeting()
