97e2a519a0003f160c4b32ccb5dabf657b31840b
zyavuz610 <43997998+zyavuz610@users.noreply.github.com>
2024-11-16 23:08:36 +0300

    Create 127-ornek-projeler.py

---
+++ 1_python-programming/youtube/100/127-ornek-projeler.py
@@ -0,0 +1,65 @@
+"""
+Seri: Örneklerle Python Programlama ve Algoritma
+
+Önceki dersler:
+  değişkenler ve operatörler
+  koşul ifadeleri: if,else
+  veri yapıları: string, liste
+  döngüler: for, while, iç içe döngüler
+  fonksiyonlar, fonksiyon parametreleri, örnekler
+  modüller, kendi modülümüzü yazmak
+  datetime, math, 
+  örnekler
+  tuple veri yapısı, tuple üzerinde işlemler
+
+Python - 127 : Örnek Projeler (önceki konuları kullanarak yapılabilen)
+
+-------------------------------------------------
+
+Problem 1:
+tp = (3,-1,4,3,6,-2,-5,-1)
+max,min product, ardışıl 2 elemanın çarpımları arasında max ve min bul
+
+-------------------------------------------------
+
+Problem 2:
+bir sayı alalım:
+
+1
+*
+
+2
+ *
+***
+
+3
+
+  *
+ ***
+*****
+
+4
+   *
+  ***
+ *****
+*******
+
+şekli ekrana yaz, alanı bul
+
+-------------------------------------------------
+
+Problem 3
+tp = (5,2,4,8,9)
+
+min-max = 2,9
+eksik sayılar: 3,6,7 ekrana yaz
+
+2:**
+3:-
+4:****
+5:*****
+6:-
+7:-
+8:********
+9:*********
+"""
